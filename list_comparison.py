79aa236c2608d0cd07048ff2c49264b83441aca4
Judit Szarvas <jusz@dtu.dk>
2019-06-28 13:56:52 +0200

    Compare lst files

---
+++ list_comparison.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+if len(sys.argv) < 2:
+    print("Usage: program <A> <B> <a:A-B|b:union|c:difference>")
+    sys.exit()
+
+Alst = []
+Blst = []
+
+with open(sys.argv[1], "r") as fp:
+    for line in fp:
+        Alst.append(line.strip())
+
+with open(sys.argv[2], "r") as fp:
+    for line in fp:
+        Blst.append(line.strip())
+
+if sys.argv[3] == "a":
+    for item in Alst:
+        if item not in Blst:
+            print(item)
+elif sys.argv[3] == "b":
+    for item in Alst:
+        if item in Blst:
+            print(item)
+elif sys.argv[3] == "c":
+    diff = []
+    for item in Alst:
+        if item not in Blst:
+            diff.append(item)
+    for item in Blst:
+        if item not in Alst:
+            diff.append(item)
+    print("\n".join(diff))
+else:
+    print("Not valid choice")
