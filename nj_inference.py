9eccd847992cde1c9f73dbdb588efa3d5068e0ee
Judit Szarvas <jusz@dtu.dk>
2021-02-01 19:34:37 +0100

    Tree option for neighbour wrapping

--- nj_inference.py
+++ nj_inference.py
@@ -225,6 +225,7 @@ parser = argparse.ArgumentParser(
     description='Calculates genetic distance between a set of input sequences')
 parser.add_argument("-seq", "--seqlist", dest="seq_file", help="Read non-homologous sequences from")
 parser.add_argument("-pre", "--prefix", dest="prefix", help="Output path prefix")
+parser.add_argument("-t", "--tree", dest="mat", help="Input phylip matrix, no distance computation")
 parser.add_argument("-a", "--allcalled", dest="allcalled", action="store_true", help="Removal of all uncertain positions")
 parser.add_argument("-s", "--probable", dest="probabilistic", action="store_true", help="Probabilistic removal of uncertain positions")
 parser.add_argument('-k', '--keep', dest="keep", action="store_true", help='Keep temporary subdirectory')
@@ -233,174 +234,198 @@ args = parser.parse_args()
 
 suffix = ""
 
-
+wdir = ""
+prefix = ""
 # File with consensus
 if args.seq_file is None:
-    exiting('No input filelist was provided')
-wdir = os.path.dirname(args.seq_file)
-if not wdir:
-    wdir = os.getcwd()
+    if args.mat is not None:
+        wdir = os.path.dirname(args.mat)
+        prefix = os.path.realpath(args.mat)
+    else:
+        exiting('No input filelist was provided')
+else:
+    wdir = os.path.dirname(args.seq_file)
+    prefix = os.path.realpath(args.seq_file)
 
 # Output prefix
-prefix = os.path.realpath(args.seq_file)
 if args.prefix is not None:
     prefix = args.prefix
 
-
-# Define nucleotides as numbers
-nuc2num = {
-   # A  adenosine       C  cytidine          G  guanine
-   # T  thymidine       N  A/G/C/T (any)     U  uridine
-   # K  G/T (keto)      S  G/C (strong)      Y  T/C (pyrimidine)
-   # M  A/C (amino)     W  A/T (weak)        R  G/A (purine)
-   # B  G/T/C           D  G/A/T             H  A/C/T
-   # V  G/C/A           -  gap of indeterminate length
-   #   A C G T
-   # A A M R W
-   # C M C S Y
-   # G R S G K
-   # T W Y K T
-   'A' : 1,
-   'T' : 2,
-   'C' : 3,
-   'G' : 4,
-   'M' : 5,
-   'R' : 6,
-   'W' : 7,
-   'S' : 8,
-   'Y' : 9,
-   'K' : 10,
-   'B' : 11,
-   'V' : 12,
-   'D' : 13
-}
-
-
-# homology reduced old isolates
-inseqs = []
-try:
-    f = open(args.seq_file, "r")
-except IOError as e:
-    exiting("Sequence file list not found.")
-for l in f:
-    l = l.strip()
-    inseqs.append(l)
-f.close()
-
-timing("# Read inputfiles from the file lists.")
-
-#TODO DONE
-slens = len(inseqs) # Number of strains in each list
-inputseqmat = None
-tot_len = None
-
-# load and encode isolates
-# might not need to be parallel (overhead)
-if slens > 30:
-    arrays = Parallel(n_jobs=no_jobs)(delayed(read_encode_univ)(isolatefile, None) for isolatefile in inseqs)
-    # dump as a memmap and concat
-    tot_len = np.shape(arrays[0])[0]
-    for i, arr in enumerate(arrays):
-        if i == 0:
-            inputseqmat = np.zeros((slens, tot_len), dtype=np.int8)
-            inputseqmat[0,:] = arr[:]
-        else:
-            inputseqmat[i,:] = arr[:]
-    del arrays[:]
-else:
-    for i, isolatefile in enumerate(inseqs):
-        if i == 0:
-            tmp_np = read_encode_univ(isolatefile, tot_len)
-            tot_len = np.shape(tmp_np)[0]
-            inputseqmat = np.zeros((slens, tot_len), dtype=np.int8)
-            inputseqmat[0,:] = tmp_np[:]
-        else:
-            inputseqmat[i,:] = read_encode_univ(isolatefile, tot_len)[:]
-
-timing("# Loaded sequence matrix into memory.")
-
-## Remove uncertain regions
-# determine masks, probabilistic, remove: False
-m = None
-if args.allcalled:
-    m = (inputseqmat != 0).all(axis=0)
-elif args.probabilistic:
-    known_frac = np.round_((inputseqmat != 0).sum(0) / slens, 5)
-    std_dev = np.std(known_frac, dtype=np.float64)
-    mean_a = np.mean(known_frac, dtype=np.float64)
-    threshold = mean_a - std_dev
-    m = (known_frac >= threshold)
-    if not args.quiet:
-        print("# Prob stats: {0:.4f} {1:.4f}".format(mean_a, std_dev), file=sys.stdout)
-else:
-    pass
-
-if m is not None:
-    inputseqmat = inputseqmat.T[m].T
-
-# conserved positions for both type of distance calculation, if there is only one volume (ie limited set size)
-cons_m = None
-if slens > 1:
-    cons_m = np.logical_not(np.all(inputseqmat == inputseqmat[0,:], axis = 0))
-    inputseqmat = inputseqmat.T[cons_m].T
-del cons_m
-
-# update lengths and isolate counts
-slens = len(inseqs)
-tot_len = np.shape(inputseqmat)[1]
-
-timing("# Removed non-informative positions from matrix.")
-# print(inseqs)
-
-
-if not args.quiet:
-    print("# Total length: %s" % (tot_len), file=sys.stdout)
-    print("# Number of strains: %s" % (slens), file=sys.stdout)
-
-
-
-# calculate genetic distance between isolates
-no_jobs = min(slens, no_jobs)
-batch_size = int(math.ceil(slens/float(no_jobs)))
-dist_calc_func = dist_calc_pw
-if args.allcalled:
-    dist_calc_func = dist_calc_all
-
-if slens > 1:
-    dist_arr = []
-    dist_arr = Parallel(n_jobs=no_jobs)(delayed(dist_calc_func)(inputseqmat, inputseqmat, i, batch_size, slens) for i in range(0,slens,batch_size))
-
-    # put it together, do a np.where on it
-    dist_o_o = dist_arr[0]
-    for np_arr in dist_arr[1:]:
-        dist_o_o = np.concatenate((dist_o_o,np_arr), axis=1)
-
-    matrix = dist_o_o.tolist()
-
-else: # only one old seq, dist 0
-    matrix = [[0]]
-
-# print(matrix)
-timing("# Calculated distances between sequences.")
-
-# print dist matrix in phylip
-outputmat = "{}.mat".format(prefix)
+outputmat = ""
 seqid2name = {}
-seq_id = ""
-seqnames = [os.path.basename(x).split(".")[0] for x in inseqs]
-with open(outputmat, "w") as matfile:
-    print("  {0}".format(len(matrix)), file=matfile)
-    for r, row in enumerate(matrix):
-        seq_id = "I{:06}".format(r+1)
-        seqid2name[seq_id] = seqnames[r]
-        print("{:<10}".format(seq_id), end = "", file=matfile)
-        for e in row[:-1]:
-            print('{0:.0f}'.format(e), end = "\t", file=matfile)
-        print('{0:.0f}'.format(row[-1]), file=matfile)
+if args.mat is None:
+    # Define nucleotides as numbers
+    nuc2num = {
+       # A  adenosine       C  cytidine          G  guanine
+       # T  thymidine       N  A/G/C/T (any)     U  uridine
+       # K  G/T (keto)      S  G/C (strong)      Y  T/C (pyrimidine)
+       # M  A/C (amino)     W  A/T (weak)        R  G/A (purine)
+       # B  G/T/C           D  G/A/T             H  A/C/T
+       # V  G/C/A           -  gap of indeterminate length
+       #   A C G T
+       # A A M R W
+       # C M C S Y
+       # G R S G K
+       # T W Y K T
+       'A' : 1,
+       'T' : 2,
+       'C' : 3,
+       'G' : 4,
+       'M' : 5,
+       'R' : 6,
+       'W' : 7,
+       'S' : 8,
+       'Y' : 9,
+       'K' : 10,
+       'B' : 11,
+       'V' : 12,
+       'D' : 13
+    }
 
-timing("# Constructed distance matrix.")
 
-timing("# Distance calculation is finished.")
+    # homology reduced old isolates
+    inseqs = []
+    try:
+        f = open(args.seq_file, "r")
+    except IOError as e:
+        exiting("Sequence file list not found.")
+    for l in f:
+        l = l.strip()
+        inseqs.append(l)
+    f.close()
+
+    timing("# Read inputfiles from the file lists.")
+
+    #TODO DONE
+    slens = len(inseqs) # Number of strains in each list
+    inputseqmat = None
+    tot_len = None
+
+    # load and encode isolates
+    # might not need to be parallel (overhead)
+    if slens > 30:
+        arrays = Parallel(n_jobs=no_jobs)(delayed(read_encode_univ)(isolatefile, None) for isolatefile in inseqs)
+        # dump as a memmap and concat
+        tot_len = np.shape(arrays[0])[0]
+        for i, arr in enumerate(arrays):
+            if i == 0:
+                inputseqmat = np.zeros((slens, tot_len), dtype=np.int8)
+                inputseqmat[0,:] = arr[:]
+            else:
+                inputseqmat[i,:] = arr[:]
+        del arrays[:]
+    else:
+        for i, isolatefile in enumerate(inseqs):
+            if i == 0:
+                tmp_np = read_encode_univ(isolatefile, tot_len)
+                tot_len = np.shape(tmp_np)[0]
+                inputseqmat = np.zeros((slens, tot_len), dtype=np.int8)
+                inputseqmat[0,:] = tmp_np[:]
+            else:
+                inputseqmat[i,:] = read_encode_univ(isolatefile, tot_len)[:]
+
+    timing("# Loaded sequence matrix into memory.")
+
+    ## Remove uncertain regions
+    # determine masks, probabilistic, remove: False
+    m = None
+    if args.allcalled:
+        m = (inputseqmat != 0).all(axis=0)
+    elif args.probabilistic:
+        known_frac = np.round_((inputseqmat != 0).sum(0) / slens, 5)
+        std_dev = np.std(known_frac, dtype=np.float64)
+        mean_a = np.mean(known_frac, dtype=np.float64)
+        threshold = mean_a - std_dev
+        m = (known_frac >= threshold)
+        if not args.quiet:
+            print("# Prob stats: {0:.4f} {1:.4f}".format(mean_a, std_dev), file=sys.stdout)
+    else:
+        pass
+
+    if m is not None:
+        inputseqmat = inputseqmat.T[m].T
+
+    # conserved positions for both type of distance calculation, if there is only one volume (ie limited set size)
+    cons_m = None
+    if slens > 1:
+        cons_m = np.logical_not(np.all(inputseqmat == inputseqmat[0,:], axis = 0))
+        inputseqmat = inputseqmat.T[cons_m].T
+    del cons_m
+
+    # update lengths and isolate counts
+    slens = len(inseqs)
+    tot_len = np.shape(inputseqmat)[1]
+
+    timing("# Removed non-informative positions from matrix.")
+    # print(inseqs)
+
+
+    if not args.quiet:
+        print("# Total length: %s" % (tot_len), file=sys.stdout)
+        print("# Number of strains: %s" % (slens), file=sys.stdout)
+
+
+
+    # calculate genetic distance between isolates
+    no_jobs = min(slens, no_jobs)
+    batch_size = int(math.ceil(slens/float(no_jobs)))
+    dist_calc_func = dist_calc_pw
+    if args.allcalled:
+        dist_calc_func = dist_calc_all
+
+    if slens > 1:
+        dist_arr = []
+        dist_arr = Parallel(n_jobs=no_jobs)(delayed(dist_calc_func)(inputseqmat, inputseqmat, i, batch_size, slens) for i in range(0,slens,batch_size))
+
+        # put it together, do a np.where on it
+        dist_o_o = dist_arr[0]
+        for np_arr in dist_arr[1:]:
+            dist_o_o = np.concatenate((dist_o_o,np_arr), axis=1)
+
+        matrix = dist_o_o.tolist()
+
+    else: # only one old seq, dist 0
+        matrix = [[0]]
+
+    # print(matrix)
+    timing("# Calculated distances between sequences.")
+
+    # print dist matrix in phylip
+    outputmat = "{}.mat".format(prefix)
+    seq_id = ""
+    seqnames = [os.path.basename(x).split(".")[0] for x in inseqs]
+    with open(outputmat, "w") as matfile:
+        print("  {0}".format(len(matrix)), file=matfile)
+        for r, row in enumerate(matrix):
+            seq_id = "I{:07}".format(r+1)
+            seqid2name[seq_id] = seqnames[r]
+            print("{:<10}".format(seq_id), end = "", file=matfile)
+            for e in row[:-1]:
+                print('{0:.0f}'.format(e), end = "\t", file=matfile)
+            print('{0:.0f}'.format(row[-1]), file=matfile)
+
+    timing("# Constructed distance matrix.")
+
+    timing("# Distance calculation is finished.")
+else:
+    outputmat = "{}.encoded.mat".format(prefix)
+    try:
+        f = open(args.mat, "r")
+    except IOError as e:
+        exiting("Phylip matrix not found.")
+    with open(outputmat, "w") as matfile:
+        firstline = f.readline()
+        matfile.write(firstline)
+        taxaindex = 0
+        for l in f:
+            if l and not l.isspace():
+                taxaindex += 1
+                taxaname, distances = l.split(None,1)
+                seq_id = "I{:07}".format(taxaindex)
+                seqid2name[seq_id] = taxaname
+                matfile.write("{:<10}".format(seq_id))
+                matfile.write(distances)
+    f.close()
 
 mode = 'pw'
 if args.allcalled:
@@ -424,7 +449,8 @@ if proc.returncode:
 timing("# Distance based tree constructed.")
 
 decode_nw_file(seqid2name, treefilename)
-decode_dist_matrix(seqid2name, outputmat, outputmat)
+if args.seq_file is not None:
+    decode_dist_matrix(seqid2name, outputmat, outputmat)
 
 try:
     shutil.copy(treefilename, "{}.nw".format(prefix))
